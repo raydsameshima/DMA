7d4461c7f583f38ecdf8bce266eedff1ee5fd2df
raydsameshima <ray.d.sameshima@gmail.com>
2020-11-16 22:39:58 -0500

    changed the indents

--- Ch02/Rot.hs
+++ Ch02/Rot.hs
@@ -1,25 +1,30 @@
 module Rot where
 
 -- https://lqtmirage.hatenablog.com/entry/2018/07/12/232852
+-- http://dis.um.es/~alberto/hmatrix/hmatrixtut.html
 
 import Prelude hiding ((<>))
 import Numeric.LinearAlgebra 
 
 
-rotX, rotY, rotZ :: Double -> Matrix Double
-rotX t = (3><3) [1.0, 0.0,    0.0
-                ,0.0, cos t,  sin t
-                ,0.0, -sin t, cos t
-                ]
-rotY t = (3><3) [cos t,  0.0, sin t
-                ,0.0,    1.0, 0.0
-                ,-sin t, 0.0, cos t
-                ]
+rotX, rotY, rotZ 
+  :: Double -> Matrix R
+rotX t = (3><3) 
+  [1.0, 0.0,    0.0
+  ,0.0, cos t,  sin t
+  ,0.0, -sin t, cos t
+  ]
+rotY t = (3><3) 
+  [cos t,  0.0, sin t
+  ,0.0,    1.0, 0.0
+  ,-sin t, 0.0, cos t
+  ]
                    
-rotZ t = (3><3) [cos t, -sin t, 0.0
-                ,sin t, cos t,  0.0
-                ,0.0,   0.0,    1.0
-                ]
+rotZ t = (3><3) 
+  [cos t, -sin t, 0.0
+  ,sin t, cos t,  0.0
+  ,0.0,   0.0,    1.0
+  ]
 
 v,w :: Vector Double
 v = vector [1.0, 1.0, 1.0]
